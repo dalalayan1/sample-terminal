1d7fbcd04b79a20f1d8c9dc2f3cdd516bd134404
Ayan Dalal <adalal4@sapient.com>
2017-02-09 02:22:36 +0530

    copy-paste divs added with blinker

--- js/index.js
+++ js/index.js
@@ -1,15 +1,49 @@
 var AT = AT || {};
 
 $(document).ready(function(){
-	$('.box').each(function() {
-	    var _this = $(this);
-	    setInterval(function() {
-	        if (_this.css('visibility') == 'hidden') {
-	            _this.css('visibility', 'visible');
-	        } else {
-	            _this.css('visibility', 'hidden');
-	        }    
+	var copyArray = $('.copy');
+	var copyText = '';
+	var paste = $('.paste');
+	var box = $('.box');
+	var count = 0;
+	var lengthArray = [];
+
+	for(var i=0;i<copyArray.length;i++){
+		if(i==copyArray.length-1){
+			copyText = copyText + $($(copyArray)[i]).text();
+		}
+		else{
+			copyText = copyText + $($(copyArray)[i]).text() + '\n';
+		}
+		
+	}
+	
+	console.log(copyText);
+	
+	var copyTextLength = copyText.length;
+
+	var	a = setInterval(function() {
+	    	if(count<=copyTextLength){
+	    			paste.append(copyText[count]);
+	    	}
+	    	else{
+	    		return;
+	    	}
+	    	count++;
+	    },100);
+
+
+	var b = setInterval(function() {
+	    	
+    			if (box.css('visibility') == 'hidden') {
+	            	box.css('visibility', 'visible');
+	        	} 
+		        else {
+		            box.css('visibility', 'hidden');
+		        } 
+	          
 	    }, 500);
-	});
+	
+
 });
 
