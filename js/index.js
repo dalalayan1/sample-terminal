73446fee15b704a7e2fa7add81498b6e70482694
Ayan Dalal <ayan.dalal@localadmins-MacBook-Pro-2.local>
2019-01-04 16:57:10 +0530

    made it functional

--- js/index.js
+++ js/index.js
@@ -1,55 +1,43 @@
 var AT = AT || {};
 
-$(document).ready(function(){
-	var copyArray = $('.copy');
-	var copyText = '';
-	var name = $('.name');
-	var paste = $('.paste');
-	var box = $('.box');
-	var count = 0;
-	var lengthArray = [];
-	var begin = 0;
-
-	function showmsg(text,i=1){
-			setTimeout(function() {
-					paste.append('<div>' + text + '</div><br>');
-				
-			}, 300*i);
-	}
-
-	function showchars(char,ind,length){
-		if(ind == 0){
-			paste.append('<span>'+char);
-		}
-		else if(ind==length-1){
-			paste.append(char+'</span>');
-		}
-		else{
-			paste.append();
-		}
-		
-	}
-
-	showmsg($(name).text());
-	var command = 'intro';
-	for(var i =0;i<command.length;i++){
-		showchars(command[i],i+1,command.length)
-	}
-	for(var i =0;i<copyArray.length;i++){
-		showmsg($(copyArray[i]).text(),i+2);
-	}
-
-	setInterval(function() {
-	    	
-    			if (box.css('visibility') == 'hidden') {
-	            	box.css('visibility', 'visible');
-	        	} 
-		        else {
-		            box.css('visibility', 'hidden');
-		        } 
-	          
-	    }, 500);
+// $(document).ready(function(){
 	
+// });
 
-});
+window.onload = function() {
+	var box = document.getElementById("box"),
+		pasteBox = document.getElementById("paste"),
+		name = document.getElementById("name"),
+		texts = Array.from(document.getElementsByClassName("copy")),
+		charsArr,
+		countIdx = 0;
 
+	texts.forEach(function(eachText, outerIdx) {
+		charsArr = Array.from(eachText.innerText);
+		charsArr.forEach(function(char, innerIdx) {
+			countIdx++;
+			setTimeout(function() {
+				pasteBox.contains(box) && pasteBox.removeChild(box);
+				pasteBox.innerHTML = pasteBox.innerHTML + char;
+				pasteBox.appendChild(box);
+				if (innerIdx === texts[outerIdx].innerHTML.length - 1) {
+					pasteBox.contains(box) && pasteBox.removeChild(box);
+					pasteBox.innerHTML = pasteBox.innerHTML + '</br>';
+					if(outerIdx == texts.length - 1) {
+						name.classList.remove("hide");
+						pasteBox.appendChild(name);
+						pasteBox.appendChild(box);
+						box.classList.add("blink");
+						acceptInputFromUser();
+					}
+				}
+			}, countIdx * 100);
+		});
+	});
+
+
+	function acceptInputFromUser() {
+		//append input to read user data
+	}
+
+};
